f8d99e26c418a4893a979592270f6fa75b3629c2
Mialbo <catalogador1@gmail.com>
2025-04-06 22:50:32 -0500

    Delete springBoot/springBoot directory

--- springBoot/springBoot/src/main/java/com/multiskill/springBoot/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.multiskill.springBoot.repository;
-
-import com.multiskill.springBoot.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
